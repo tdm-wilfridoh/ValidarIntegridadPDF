c4983be74db30e9fd1aa160cf105c2a56ad2306d
tdm-wilfridoh <99975188+tdm-wilfridoh@users.noreply.github.com>
2024-05-23 23:48:14 -0500

    Acceso al repositorio por medio de la ApiLF

---
+++ ExtensionsMethods/WorkBookExtensions.cs
@@ -0,0 +1,104 @@
+﻿using iTextSharp.text;
+using NPOI.HSSF.Util;
+using NPOI.SS.UserModel;
+using Org.BouncyCastle.Utilities.Collections;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tdm.APILF.Domain.Models;
+using static iTextSharp.text.pdf.AcroFields;
+using Document = Tdm.APILF.Domain.Models.Document;
+
+namespace ValidarIntegridadPDF.ExtensionsMethods
+{
+    public static class WorkBookExtensions
+    {
+        public static int numeroFila = 0;
+        
+
+        public static ISheet CrearEncabezado(this IWorkbook wb)
+        {
+
+            // create a new worksheet
+            ISheet ws = wb.CreateSheet("Documentos corruptos");
+
+            //Create style
+            //ICellStyle style = wb.CreateCellStyle();
+            ////Set border style 
+            ////style.BorderBottom = BorderStyle.Double;
+            ////style.BottomBorderColor = HSSFColor.Black.Index;
+            ////Set font style
+            //IFont font = wb.CreateFont();
+            ////font.Color = HSSFColor.White.Index;
+            //font.FontName = "Arial";
+            //font.FontHeight = 11;
+            ////font.Boldweight = 2;
+            //style.SetFont(font);
+            //Set background color
+            //style.FillBackgroundColor = IndexedColors.BlueGrey.Index;
+            //style.FillPattern = FillPattern.SolidForeground;
+
+            // create a new row
+            IRow row = ws.CreateRow(numeroFila);
+
+            // Encabezado "ID"
+            ICell encabezadoID = row.CreateCell(0);
+            encabezadoID.SetCellValue("ID");
+            //Apply the style
+            //encabezadoID.CellStyle = style;
+
+            // Encabezado "NOMBRE"
+            ICell encabezadoNombre = row.CreateCell(1);
+            encabezadoNombre.SetCellValue("NOMBRE");
+            //Apply the style
+            //encabezadoNombre.CellStyle = style;
+
+            // Encabezado "RUTA"
+            ICell encabezadoRuta = row.CreateCell(2);
+            encabezadoRuta.SetCellValue("RUTA");
+            //Apply the style
+            //encabezadoRuta.CellStyle = style;
+
+            // Encabezado "FECHA DE CREACIÓN"
+            ICell encabezadoFechaCreacion = row.CreateCell(3);
+            encabezadoFechaCreacion.SetCellValue("FECHA DE CREACIÓN");
+            //Apply the style
+            //encabezadoFechaCreacion.CellStyle = style;
+
+            return ws;
+        }
+
+        public static void AdicionarFila(this ISheet ws, Document document)
+        {
+
+            // create a new row
+            IRow row = ws.CreateRow(++numeroFila);
+
+            // create a new cell and set its value
+            ICell celdaTabla = row.CreateCell(0);
+            celdaTabla.SetCellValue(document.ID);
+
+            ICell celdaCaja = row.CreateCell(1);
+            celdaCaja.SetCellValue(document.Name);
+
+            ICell celdaEncontrado = row.CreateCell(2);
+            celdaEncontrado.SetCellValue(document.Path);
+
+            ICell celdaFolderCode = row.CreateCell(3);
+            celdaFolderCode.SetCellValue(document.CreationDate.ToString());
+        }
+
+        public static void GuardarReporte(this IWorkbook wb, string path)
+        {
+            using (var archivoResultado = new FileStream(path, FileMode.Create, FileAccess.Write))
+            {
+                wb.Write(archivoResultado);
+            }
+        }
+
+        public static bool ExistenArchivosCorruptos(this ISheet ws) => numeroFila > 0;
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,181 @@
+﻿using iTextSharp.text.pdf;
+using Newtonsoft.Json;
+using NPOI.HSSF.Record;
+using NPOI.SS.UserModel;
+using NPOI.XSSF.UserModel;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using System.Reflection.Metadata;
+using System.Reflection.PortableExecutable;
+using Tdm.APILF.Application.Services;
+using Tdm.APILF.Domain.Models;
+using ValidarIntegridadPDF.ExtensionsMethods;
+using Document = Tdm.APILF.Domain.Models.Document;
+
+internal class Program
+{
+    private static HttpClient client = new HttpClient();
+
+    private static string exportPath = "C:\\WorkSpace\\TGI\\ValidarIntegridadPDF\\bin\\docs";
+
+    private static string reportPath = "C:\\WorkSpace\\TGI\\ValidarIntegridadPDF\\bin\\report\\Archivos_Corruptos.xlsx";
+
+
+    private static void Main(string[] args)
+    {
+        client.BaseAddress = new Uri("https://localhost:44395/");
+        client.DefaultRequestHeaders.Accept.Clear();
+        client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        client.Timeout = new TimeSpan(1, 0, 0);
+
+        InitAsync().GetAwaiter().GetResult();
+        Console.ReadLine();
+    }
+
+    private static async Task InitAsync()
+    {
+        SearchByCreationDateRequest request = new SearchByCreationDateRequest()
+        {
+            InitialDate = "2018/06/22 00:00:00",
+            FinalDate = "2018/06/22 15:00:00"
+        };
+
+        var response = await client.PostAsJsonAsync("api/ApiLF/SearchByCreationDate", request);
+        List<Document> documentos = new List<Document>();
+
+        if (response.IsSuccessStatusCode)
+        {
+            var result = await response.Content.ReadAsAsync<ApiResponse<SearchResult>>();
+            if(result.Succeded)
+            {
+                documentos = result.Data.Documents;
+                await ExportarDocumentosAsync(documentos);
+            }
+            else
+                await Console.Out.WriteLineAsync("Error la consultar la API: " + result.ErrorMessage);
+        }
+
+    }
+
+    private static async Task ExportarDocumentosAsync(List<Document> docs)
+    {
+        int i = 1;
+
+        //for (int i = 0; i< docs.Count; i++)
+        //{
+        //    if (i+1 % 10 == 0)
+        //        Thread.Sleep(1000);
+
+        //    var doc = docs[i];
+
+
+        //    var response = await client.GetAsync($"api/ApiLF/export/{doc.ID}");
+        //    if (response.IsSuccessStatusCode)
+        //    {
+
+        //        var result = await response.Content.ReadAsAsync<ApiResponse<ExportResult>>();
+
+
+        //        //if (i > 0 && docs[i-1].Name == doc.Name)
+
+        //        if (BuscarArchivo(result.Data.FileName))
+        //        {
+        //            /*Tamaño del nombre del documento*/
+        //            var lengthDocumentName = doc.Path.Split('\\').Last().Length;
+
+        //            /*Ruta en LF donde se encuentra el documento*/
+        //            var lfDirectory = doc.Path.Substring(1, doc.Path.Length - (lengthDocumentName + 2));
+
+        //            var newDirectory = $"{exportPath}\\{doc.Name}\\{lfDirectory}";
+        //            Directory.CreateDirectory(newDirectory);
+        //            File.WriteAllBytes($"{newDirectory}\\{result.Data.FileName}", result.Data.Stream);
+
+
+        //        }
+        //        else
+        //            File.WriteAllBytes($"{exportPath}\\{result.Data.FileName}", result.Data.Stream);
+
+        //    }
+        //}
+
+        // create a new workbook
+        IWorkbook wb = new XSSFWorkbook();
+
+        ISheet ws = wb.CrearEncabezado();
+
+
+        foreach(var doc in docs)
+        {
+            if (i++ % 10 == 0)
+                Thread.Sleep(1000);
+
+            var response = await client.GetAsync($"api/ApiLF/export/{doc.ID}");
+
+            if (response.IsSuccessStatusCode)
+            {
+
+                string filePath;
+
+                var result = response.Content.ReadAsAsync<ApiResponse<ExportResult>>().Result;
+
+                if (!result.Succeded)
+                {
+                    Console.WriteLine($"Error al exportar - ID {doc.ID}: {result.ErrorMessage}");
+                    continue;
+                }
+
+                //if (i > 0 && docs[i-1].Name == doc.Name)
+
+                if (BuscarArchivo(result.Data.FileName))
+                {
+                    /*Tamaño del nombre del documento*/
+                    var lengthDocumentName = doc.Path.Split('\\').Last().Length;
+
+                    /*Ruta en LF donde se encuentra el documento*/
+                    var lfDirectory = doc.Path.Substring(1, doc.Path.Length - (lengthDocumentName + 2));
+
+                    var newDirectory = $"{exportPath}\\{doc.Name}\\{lfDirectory}";
+                    Directory.CreateDirectory(newDirectory);
+                    filePath = $"{newDirectory}\\{result.Data.FileName}";
+                    File.WriteAllBytes(filePath, result.Data.Stream);
+
+
+                }
+                else
+                {
+                    filePath = $"{exportPath}\\{result.Data.FileName}";
+                    File.WriteAllBytes(filePath, result.Data.Stream);
+                }
+
+                if (!ValidarIntegridad(filePath))
+                {
+                    ws.AdicionarFila(doc);
+                }
+
+            }
+        }
+
+        if(ws.ExistenArchivosCorruptos())
+        {
+            wb.GuardarReporte(reportPath);
+        }
+    }
+
+    //Validar si existe un documento con el mismo nombre en la ruta de exportación
+    private static bool BuscarArchivo(string file) => Directory.GetFiles(exportPath, file, SearchOption.TopDirectoryOnly).Length > 0;
+
+    private static bool ValidarIntegridad(string file)
+    {
+        try
+        {
+            using PdfReader reader = new PdfReader(file);
+        }
+        catch(Exception e)
+        {
+            Console.WriteLine($"Error Validar Integridad  -  {file}: {e.Message}");
+            return false;
+        }
+
+        return true;
+    }
+}
